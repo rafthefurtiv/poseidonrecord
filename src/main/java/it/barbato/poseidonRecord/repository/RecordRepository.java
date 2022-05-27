68865f9846b46d67e49da55820816c56b462c90c
rafthefurtiv <rafthefurtiv@gmail.com>
2022-05-27 17:45:24 +0200

    f

--- src/main/java/it/barbato/poseidonRecord/repository/RecordRepository.java
+++ src/main/java/it/barbato/poseidonRecord/repository/RecordRepository.java
@@ -22,12 +22,14 @@ public interface RecordRepository extends MineRepository<Record, Integer> {
             "group by r.stile, r.metri, r.flag_vasca_corta, r.categoria, u2.codice_sesso \n" +
             ")\n" +
             "as t on t.tempo = r2.tempo  \n" +
+            "AND r2.stile = t.stile  \n" +
             "AND r2.metri = t.metri  \n" +
             "AND r2.flag_vasca_corta = t.flag_vasca_corta  \n" +
             "AND r2.categoria = t.categoria  \n" +
             "inner join utenti u on r2.utente = u.id_utente and t.codice_sesso = u.codice_sesso \n" +
             "inner join categorie c on c.id_categoria = r2.categoria \n" +
-            "inner join stili s on s.id_stile = r2.stile \n", nativeQuery = true)
+            "inner join stili s on s.id_stile = r2.stile \n" +
+            "order by stile, metri \n", nativeQuery = true)
     public List<Record> findSocietariAll();
 
 
