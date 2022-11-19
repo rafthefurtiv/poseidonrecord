be041e550a4cceb8019e6709692bf6a74bfe5aff
Raffaele <rafthefurtiv@hotmail.it>
2022-11-19 17:18:54 +0100

    Step 2

--- src/main/resources/SQL/MEV_MACCHINE_18_10_2022/Macchine_MEV.sql
+++ src/main/resources/SQL/MEV_MACCHINE_18_10_2022/Macchine_MEV.sql
@@ -1,8 +1,8 @@
-drop table poseidonRecord.Macchine;
-drop table poseidonRecord.Macchine_utenti;
+drop table poseidonRecord.macchine;
+drop table poseidonRecord.macchine_utenti;
 
 
-CREATE TABLE poseidonRecord.Macchine (
+CREATE TABLE poseidonRecord.macchine (
 	id_macchina INT NOT null primary key AUTO_INCREMENT,
 	nome VARCHAR(100) NOT NULL,
 	posti_andata INT not null,
@@ -19,21 +19,14 @@ CREATE TABLE poseidonRecord.Macchine (
 
 
 
-CREATE TABLE poseidonRecord.Macchine_utenti (
+CREATE TABLE poseidonRecord.macchine_utenti (
 	id_macchina_utente INT NOT null primary key AUTO_INCREMENT,
 	macchina INT not null,
 	passeggero INT NOT NULL,
 	andata boolean not null,
 	ritorno boolean not null,
 		  CONSTRAINT `fk_macchina`
-    FOREIGN KEY (macchina) REFERENCES Macchine (id_macchina),
+    FOREIGN KEY (macchina) REFERENCES macchine (id_macchina),
     		  CONSTRAINT `fk_macchina_utente_passeggero`
     FOREIGN KEY (passeggero) REFERENCES utenti (id_utente)
 );
-
-
-
-
-
-
-
